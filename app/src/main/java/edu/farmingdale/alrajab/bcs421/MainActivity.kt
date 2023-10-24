077d005686cc4a9b90558baf42d041548d2c754e
Rahim Akhter <akhter.s.rahim@gmail.com>
2023-10-24 10:27:32 -0400

    Complete SP Task

--- app/src/main/java/edu/farmingdale/alrajab/bcs421/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/alrajab/bcs421/MainActivity.kt
@@ -6,46 +6,56 @@ import android.os.Bundle
 import edu.farmingdale.alrajab.bcs421.ui.database.DatabaseActivity
 import edu.farmingdale.alrajab.bcs421.databinding.ActivityMainBinding
 import edu.farmingdale.alrajab.bcs421.ui.files.FileActivity
+import edu.farmingdale.alrajab.bcs421.ui.sp.SPActivity
 
 class MainActivity : AppCompatActivity() {
 
-    // create binding view true - make sure to add it to the module build.gradle
-    private lateinit var binding: ActivityMainBinding
+	// create binding view true - make sure to add it to the module build.gradle
+	private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
-        binding.databaseBtn.setOnClickListener {  processDatabase()}
+		binding.databaseBtn.setOnClickListener { processDatabase() }
 
-        binding.filesBtn.setOnClickListener { processFiles() }
+		binding.filesBtn.setOnClickListener { processFiles() }
 
-        // TODO 01:SP Add another button for saving data using Shared Preferences
-        // TODO 02:SP Make an activity that accept the User's first and last name and save/read/update
-        //  the shared preference
+		// Done 01:SP Add another button for saving data using Shared Preferences
+		binding.buttonSp.setOnClickListener { processSharedPreference() }
 
-        // TODO 03:DB Edit the Database page so that you store  the first name and last name of the
-        //  user input (EditView)
+		// Done 02:SP Make an activity that accept the User's first and last name and save/read/update
+		//  the shared preference
 
-        // TODO 04:DB Update an existing name with a new one
-        // TODO 05:DB Read from the files and write to the DB
+		// TODO 03:DB Edit the Database page so that you store  the first name and last name of the
+		//  user input (EditView)
 
-        // TODO 06: Push your code to GitHub and submit the link
+		// TODO 04:DB Update an existing name with a new one
+		// TODO 05:DB Read from the files and write to the DB
 
-    }
+		// TODO 06: Push your code to GitHub and submit the link
 
-    /**
-     * Move to the file activity
-     */
-    private fun processFiles() {
-        startActivity( Intent(this, FileActivity::class.java) )
-    }
+	}
 
-    /**
-     * Move to the database activity
-     */
-    private fun processDatabase() {
-        startActivity( Intent(this, DatabaseActivity::class.java) )
-    }
+	/**
+	 * Move to the file activity
+	 */
+	private fun processFiles() {
+		startActivity(Intent(this, FileActivity::class.java))
+	}
+
+	/**
+	 * Move to the database activity
+	 */
+	private fun processDatabase() {
+		startActivity(Intent(this, DatabaseActivity::class.java))
+	}
+
+	/**
+	 * Move to the shared preference activity
+	 */
+	private fun processSharedPreference() {
+		startActivity(Intent(this, SPActivity::class.java))
+	}
 }
--- app/src/main/java/edu/farmingdale/alrajab/bcs421/ui/sp/SPActivity.kt
+++ app/src/main/java/edu/farmingdale/alrajab/bcs421/ui/sp/SPActivity.kt
@@ -1,46 +1,124 @@
 package edu.farmingdale.alrajab.bcs421.ui.sp
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.edit
 import edu.farmingdale.alrajab.bcs421.ui.theme.DataPersistenceTheme
 
 class SPActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			DataPersistenceTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(
-					modifier = Modifier.fillMaxSize(),
-					color = MaterialTheme.colorScheme.background
-				) {
-					Greeting("Android")
-				}
-			}
+			SPView()
 		}
 	}
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
+fun SPView() {
+	val context = LocalContext.current
+	val sp = remember { context.getSharedPreferences("User", Context.MODE_PRIVATE) }
+
+	DataPersistenceTheme {
+		var givenName: String by remember { mutableStateOf("") }
+		var familyName: String by remember { mutableStateOf("") }
+
+		Scaffold {
+			SPContent(
+				givenName = givenName,
+				onUpdateGivenName = { givenName = it },
+				familyName = familyName,
+				onUpdateFamilyName = { familyName = it },
+				save = {
+					sp.edit {
+						putString("name_given", givenName)
+						putString("name_family", familyName)
+					}
+				},
+				read = {
+					givenName = sp.getString("name_given", "") ?: ""
+					givenName = sp.getString("name_family", "") ?: ""
+				},
+				modifier = Modifier.padding(it)
+			)
+		}
+	}
+}
+
+@Preview
+@Composable
+fun PreviewSPContent() {
+	SPContent(
+		"A",
+		{},
+		"B",
+		{},
+
+		{},
+		{}
 	)
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-	DataPersistenceTheme {
-		Greeting("Android")
+fun SPContent(
+	givenName: String,
+	onUpdateGivenName: (String) -> Unit,
+	familyName: String,
+	onUpdateFamilyName: (String) -> Unit,
+
+	save: () -> Unit,
+	read: () -> Unit,
+
+	modifier: Modifier = Modifier
+) {
+	Column(modifier) {
+		OutlinedTextField(
+			givenName,
+			onUpdateGivenName,
+			label = {
+				Text("Given Name")
+			}
+		)
+
+		OutlinedTextField(
+			familyName,
+			onUpdateFamilyName,
+			label = {
+				Text("Family Name")
+			}
+		)
+
+		Row {
+			Button(save) {
+				Text("Save")
+			}
+			Button(read) {
+				Text("Read")
+			}
+		}
 	}
 }
