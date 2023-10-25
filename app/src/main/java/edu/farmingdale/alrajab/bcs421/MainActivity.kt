24d541a1c933c94e55e272eab0e0091ac467ab00
Rahim Akhter <akhter.s.rahim@gmail.com>
2023-10-25 12:26:35 -0400

    Convert MainView to Compose

--- app/src/main/java/edu/farmingdale/alrajab/bcs421/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/alrajab/bcs421/MainActivity.kt
@@ -3,59 +3,108 @@ package edu.farmingdale.alrajab.bcs421
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import edu.farmingdale.alrajab.bcs421.ui.database.DatabaseActivity
-import edu.farmingdale.alrajab.bcs421.databinding.ActivityMainBinding
 import edu.farmingdale.alrajab.bcs421.ui.files.FileActivity
 import edu.farmingdale.alrajab.bcs421.ui.sp.SPActivity
+import edu.farmingdale.alrajab.bcs421.ui.theme.DataPersistenceTheme
 
 class MainActivity : AppCompatActivity() {
 
-	// create binding view true - make sure to add it to the module build.gradle
-	private lateinit var binding: ActivityMainBinding
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		binding = ActivityMainBinding.inflate(layoutInflater)
-		setContentView(binding.root)
-
-		binding.databaseBtn.setOnClickListener { processDatabase() }
-
-		binding.filesBtn.setOnClickListener { processFiles() }
-
-		// Done 01:SP Add another button for saving data using Shared Preferences
-		binding.buttonSp.setOnClickListener { processSharedPreference() }
-
-		// Done 02:SP Make an activity that accept the User's first and last name and save/read/update
-		//  the shared preference
-
-		// Done 03:DB Edit the Database page so that you store  the first name and last name of the
-		//  user input (EditView)
-
-		// Done 04:DB Update an existing name with a new one
-		// TODO 05:DB Read from the files and write to the DB
-
-		// TODO 06: Push your code to GitHub and submit the link
-
+		setContent {
+			MainView()
+		}
 	}
+}
 
-	/**
-	 * Move to the file activity
-	 */
-	private fun processFiles() {
-		startActivity(Intent(this, FileActivity::class.java))
+// Done 01:SP Add another button for saving data using Shared Preferences
+
+// Done 02:SP Make an activity that accept the User's first and last name and save/read/update
+//  the shared preference
+
+// Done 03:DB Edit the Database page so that you store  the first name and last name of the
+//  user input (EditView)
+
+// Done 04:DB Update an existing name with a new one
+// TODO 05:DB Read from the files and write to the DB
+
+// TODO 06: Push your code to GitHub and submit the link
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainView() {
+	val context = LocalContext.current
+
+	DataPersistenceTheme {
+		MainContent(
+			goToFiles = {
+				context.startActivity(Intent(context, FileActivity::class.java))
+			},
+
+			goToDb = {
+				context.startActivity(Intent(context, DatabaseActivity::class.java))
+			},
+
+			goToSp = {
+				context.startActivity(Intent(context, SPActivity::class.java))
+			}
+		)
 	}
+}
 
-	/**
-	 * Move to the database activity
-	 */
-	private fun processDatabase() {
-		startActivity(Intent(this, DatabaseActivity::class.java))
+@Preview
+@Composable
+fun PreviewMainContent() {
+	DataPersistenceTheme {
+		MainContent(
+			{},
+			{},
+			{}
+		)
 	}
+}
 
-	/**
-	 * Move to the shared preference activity
-	 */
-	private fun processSharedPreference() {
-		startActivity(Intent(this, SPActivity::class.java))
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainContent(
+	goToFiles: () -> Unit,
+	goToDb: () -> Unit,
+	goToSp: () -> Unit
+) {
+	Scaffold { paddingValues ->
+		Column(Modifier.padding(paddingValues)) {
+			Button(
+				goToFiles,
+				Modifier.fillMaxWidth()
+			) {
+				Text("Files")
+			}
+			Button(
+				goToDb,
+				Modifier.fillMaxWidth()
+			) {
+				Text("Database")
+			}
+			Button(
+				goToSp,
+				Modifier.fillMaxWidth()
+			) {
+				Text("Shared Preferences")
+			}
+		}
 	}
 }
