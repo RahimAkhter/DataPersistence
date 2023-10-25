838a508a048ce96946080c2960ccb0d6ea4d8fd2
Rahim Akhter <akhter.s.rahim@gmail.com>
2023-10-25 12:37:31 -0400

    Fix FileActivity crashes

--- app/src/main/java/edu/farmingdale/alrajab/bcs421/ui/files/FileActivity.kt
+++ app/src/main/java/edu/farmingdale/alrajab/bcs421/ui/files/FileActivity.kt
@@ -1,50 +1,58 @@
 package edu.farmingdale.alrajab.bcs421.ui.files
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import edu.farmingdale.alrajab.bcs421.MainActivity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import edu.farmingdale.alrajab.bcs421.databinding.ActivityFileBinding
+import java.io.FileNotFoundException
 import java.io.PrintWriter
 
 class FileActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityFileBinding
+	private lateinit var binding: ActivityFileBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityFileBinding.inflate(layoutInflater )
-        setContentView(binding.root)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityFileBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
 
-        binding.writeToFile.setOnClickListener { writeToInternalFile() }
-        binding.readFromFile.setOnClickListener { readFromInternalFile() }
+		binding.writeToFile.setOnClickListener { writeToInternalFile() }
+		binding.readFromFile.setOnClickListener { readFromInternalFile() }
 
-        binding.goBackBtn.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-    }
+		binding.goBackBtn.setOnClickListener {
+			finish()
+		}
+	}
 
-    private fun readFromInternalFile() {
-        val inputStream = openFileInput("myfile")
-        val reader = inputStream.bufferedReader()
-        val stringBuilder = StringBuilder()
-        val lineSeparator = System.getProperty("line.separator")
+	private fun readFromInternalFile() {
+		try {
+			val inputStream = openFileInput("myfile")
+			val reader = inputStream.bufferedReader()
+			val stringBuilder = StringBuilder()
+			val lineSeparator = System.getProperty("line.separator")
 
-        // Append each task to stringBuilder
-        reader.forEachLine { stringBuilder.append(it).append(lineSeparator) }
+			// Append each task to stringBuilder
+			reader.forEachLine { stringBuilder.append(it).append(lineSeparator) }
 
-        binding.textOfFile.text = stringBuilder.toString()
-    }
+			binding.textOfFile.text = stringBuilder.toString()
+		} catch (e: FileNotFoundException) {
+			Toast.makeText(this, "No file!", Toast.LENGTH_SHORT).show()
+		}
+	}
 
-    private fun writeToInternalFile() {
-        val outputStream = openFileOutput("myfile", MODE_PRIVATE)
-        val writer = PrintWriter(outputStream)
+	private fun writeToInternalFile() {
+		try {
+			val outputStream = openFileOutput("myfile", MODE_PRIVATE)
+			val writer = PrintWriter(outputStream)
 
-        // Write each task on a separate line
-        writer.println(binding.enterValue.text)
+			// Write each task on a separate line
+			writer.println(binding.enterValue.text)
 
-        writer.close()
-    }
+			writer.close()
+		} catch (e: FileNotFoundException) {
+			Toast.makeText(this, "No file!", Toast.LENGTH_SHORT).show()
+		}
+	}
 
 }
