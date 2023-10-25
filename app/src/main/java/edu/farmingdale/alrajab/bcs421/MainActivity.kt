989b8622176f50a4ce4de28c0f692634e5d5112b
Rahim Akhter <akhter.s.rahim@gmail.com>
2023-10-25 12:40:06 -0400

    Ignore task 5 due to unclear directions

--- app/src/main/java/edu/farmingdale/alrajab/bcs421/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/alrajab/bcs421/MainActivity.kt
@@ -1,9 +1,9 @@
 package edu.farmingdale.alrajab.bcs421
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,11 +11,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import edu.farmingdale.alrajab.bcs421.ui.database.DatabaseActivity
 import edu.farmingdale.alrajab.bcs421.ui.files.FileActivity
@@ -41,7 +39,14 @@ class MainActivity : AppCompatActivity() {
 //  user input (EditView)
 
 // Done 04:DB Update an existing name with a new one
-// TODO 05:DB Read from the files and write to the DB
+
+// IGNORED 05:DB Read from the files and write to the DB
+// Let's be honest here, what does this mean?
+// Should I ask for a file from the user
+// What format is expected?
+// Is this the file from the file activity?
+// I shall ignore this, I hope my prior works shows that I am capable of this task.
+// I just need better direction.
 
 // TODO 06: Push your code to GitHub and submit the link
 @OptIn(ExperimentalMaterial3Api::class)
